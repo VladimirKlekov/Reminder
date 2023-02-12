a1e71dcb43ab02f04feb2d9e6fab6f6f8f12b938
Vladimir Klekov <vladimirklekovgm@gmail.com>
2023-02-13 00:34:26 +0300

    сделал шаблон активити

---
+++ app/src/main/java/com/example/reminder/dto/User.kt
@@ -0,0 +1,9 @@
+package com.example.reminder.dto
+
+data class User (
+    val id: Int,
+    val lastName: String,
+    val firstName: String,
+    val middleName: String,
+    val dateBirth: Long
+        )
